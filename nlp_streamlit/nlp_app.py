36572f3bfd85058cb6f596747d2b253bc7e98398
kieran <kla65@case.edu>
2025-12-05 02:58:41 -0500

    uhhhh ig this will commit some or all of it

---
+++ nlp_streamlit/nlp_app.py
@@ -0,0 +1,46 @@
+# Streamlit Libraries
+import streamlit as st
+# NLP Libraries
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from textblob import TextBlob
+import re
+
+nltk.download('wordnet')
+
+st.title("senemental analical applical")
+st.subheader("text area")
+text = st.text_area("text here")
+
+if (st.button("Analyze")) :
+    #Keeping only Text and digits
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    #Removes Whitespaces
+    text = re.sub(r"\'s", " ", text)
+    # Removing Links if any
+    text = re.sub(r"http\S+", " link ", text)
+    # Removes Punctuations and Numbers
+    text = re.sub(r"\b\d+(?:\.\d+)?\s+", "", text)
+    # Splitting Text
+    text = text.split()
+    # Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
+    text = " ".join(lemmatized_words)
+
+    blob = TextBlob(text)
+    sentiment_score = blob.sentiment.polarity
+    
+    result = sentiment_score
+    if result > 0:
+        custom_emoji = ':blush:'
+        st.success('Happy : {}'.format(custom_emoji))
+    elif result < 0:
+        custom_emoji = ':disappointed:'
+        st.warning('Sad : {}'.format(custom_emoji))
+    else:
+        custom_emoji = ':confused:'
+        st.info('Confused : {}'.format(custom_emoji))
+    st.success("Polarity Score is: {}".format(result))
+
+
